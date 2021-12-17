aca3c69502bf04d7b387bf0384261670f071bceb
silenc3502 <gcccompil3r@gmail.com>
2021-12-17 16:11:00 +0900

    Init Web Repo

---
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/SmartcityBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.eddi.smartcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcityBackendApplication.class, args);
+    }
+
+}
