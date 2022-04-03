1582a20afe781139f192d98fea092502b2d0af99
silenc3502 <gcccompil3r@gmail.com>
2022-04-03 19:58:54 +0900

    feat: 프로토콜 콜 모듈 초기화 완료 [PROTO-1]

--- smart_city_socket_server/thread/prot_proc_thread.c
+++ smart_city_socket_server/thread/prot_proc_thread.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,18 +8,24 @@
 #include "prot_proc_thread.h"
 #include "common.h"
 
+#include "protocol_handler.h"
+#include "protocol.h"
+
 extern pthread_mutex_t mtx;
 extern pthread_cond_t edge_recv_cond_mtx;
 
 void *encrypt_side_protocol_processer (void *fd)
 {
-    int command;
-    void *pkt = NULL;
+    void *pkt = (protocol_packt *)malloc(sizeof(protocol_packt));
 
     for(;;)
     {
         pthread_mutex_lock(&mtx);
         pthread_cond_wait(&edge_recv_cond_mtx, &mtx);
+
+        memcpy(pkt, encrypt_side_sock_buf, sizeof(protocol_packt));
+
+        protocol_call_table[((protocol_packt *)pkt)->target_command](pkt);
 
         pthread_mutex_unlock(&mtx);
 
