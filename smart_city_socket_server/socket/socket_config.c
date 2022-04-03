d7e096a292b7cf591f575a15daabc86f58f36749
silenc3502 <gcccompil3r@gmail.com>
2022-04-03 19:16:56 +0900

    feat: Edge Server TCP -> UDP changes, Conditional Mutex Init [EDGSER-1]

--- smart_city_socket_server/socket/socket_config.c
+++ smart_city_socket_server/socket/socket_config.c
@@ -11,28 +11,30 @@
 
 #include "socket_config.h"
 
-//void socket_config (int *sc, si *sa, int sa_size, char *port)
 void socket_config (int *sc, si *sa, int sa_size, int port)
 {
     int flag;
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
 
     if(serv_sock == -1)
         err_handler((char *)"socket() error");
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
+    addrlen = sizeof(struct sockaddr);
+
+    memset(&clnt_addr, 0, addrlen);
+    memset(&serv_addr, 0, addrlen);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    //serv_addr.sin_port = htons(atoi(port));
     serv_addr.sin_port = htons(port);
 
     if(bind(serv_sock, (sp)&serv_addr, sizeof(serv_addr)) == -1)
         err_handler((char *)"bind() error");
 
+#if TCP
     if(listen(serv_sock, 1) == -1)
         err_handler((char *)"listen() error");
-
-    addr_size = sizeof(clnt_addr);
+#else
+#endif
 
     //flag = fcntl(serv_sock, F_GETFL, 0);
     //fcntl(serv_sock, F_SETFL, flag | O_NONBLOCK);
