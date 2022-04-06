657d63c2525895cc47751e4c5bd1b35e3c557d08
silenc3502 <gcccompil3r@gmail.com>
2022-04-06 13:35:18 +0900

    init: Traffic Control System tms570 초기화

---
+++ traffic_control_system/TMS570LC4357_FreeRTOS_LWIP/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"/home/oem/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/lib/rtsv7R4_T_be_v3D16_eabi.lib" -lrtsv7R4_T_be_v3D16_eabi.lib
+
