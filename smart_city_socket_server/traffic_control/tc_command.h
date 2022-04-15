e9460359cd650968e2bb8afebf9a759c098cf202
silenc3502 <gcccompil3r@gmail.com>
2022-04-16 02:33:33 +0900

    feat: 교통 통제 핸들러 구현 및 프로토콜 변경 [EDGSER-2]

---
+++ smart_city_socket_server/traffic_control/tc_command.h
@@ -0,0 +1,22 @@
+#ifndef SMART_CITY_SOCKET_SERVER_TC_COMMAND_H
+#define SMART_CITY_SOCKET_SERVER_TC_COMMAND_H
+
+enum eddi_smartcity_traffic_control_protocol {
+    TC_BARRICADE,
+    TC_LIFT,
+    TC_STREET_LAMP,
+    TC_TRAFFIC_LIGHT,
+    TC_END
+};
+
+#define TC_CALL_BUFFER_COUNT          (TC_END)
+#define TC_CALL_BUFFER                ((TC_END) - (1))
+
+typedef void (* tc_table_ptr_t) (void *);
+
+const tc_table_ptr_t tc_command_table[TC_CALL_BUFFER_COUNT] = {
+        [0 ... TC_CALL_BUFFER] = NULL,
+#include "tc_command_handler.h"
+};
+
+#endif //SMART_CITY_SOCKET_SERVER_TC_COMMAND_H
