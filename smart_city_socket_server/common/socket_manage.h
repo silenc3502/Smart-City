29128bef14f7f004e3a2891d96ad11dcb5f04662
silenc3502 <gcccompil3r@gmail.com>
2022-04-20 14:06:20 +0900

    feat: 세션 관리 추가 [EDGSER-5]

---
+++ smart_city_socket_server/common/socket_manage.h
@@ -0,0 +1,15 @@
+#ifndef SMART_CITY_SOCKET_SERVER_SOCKET_MANAGE_H
+#define SMART_CITY_SOCKET_SERVER_SOCKET_MANAGE_H
+
+typedef struct _socket_manage socket_manage;
+
+struct _socket_manage
+{
+    int service_code;   // target_command
+    int session_id;
+    int alloc_fd;
+};
+
+socket_manage socket_manage_map[10][20];
+
+#endif //SMART_CITY_SOCKET_SERVER_SOCKET_MANAGE_H
