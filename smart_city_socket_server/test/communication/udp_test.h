f5d87c8c4f861c6f2009279b50677ea03135370c
silenc3502 <gcccompil3r@gmail.com>
2022-04-20 12:23:11 +0900

    feat: 데이터 수신 테스트 완료 [GTEST-2]

---
+++ smart_city_socket_server/test/communication/udp_test.h
@@ -0,0 +1,16 @@
+#ifndef SMART_CITY_SOCKET_SERVER_UDP_TEST_H
+#define SMART_CITY_SOCKET_SERVER_UDP_TEST_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "common.h"
+#include "protocol.h"
+#include "tc_command.h"
+
+#endif //SMART_CITY_SOCKET_SERVER_UDP_TEST_H
