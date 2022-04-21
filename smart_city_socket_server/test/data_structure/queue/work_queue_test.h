02ec47697d409633672506631cb6e0e7032fa326
silenc3502 <gcccompil3r@gmail.com>
2022-04-21 12:39:07 +0900

    feat: Queue 동작 테스트 [GTEST-4]

---
+++ smart_city_socket_server/test/data_structure/queue/work_queue_test.h
@@ -0,0 +1,17 @@
+#ifndef SMART_CITY_SOCKET_SERVER_WORK_QUEUE_TEST_H
+#define SMART_CITY_SOCKET_SERVER_WORK_QUEUE_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "thread_work_queue.h"
+#include "socket_manage.h"
+
+void print_work_queue (work_queue *head);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //SMART_CITY_SOCKET_SERVER_WORK_QUEUE_TEST_H
