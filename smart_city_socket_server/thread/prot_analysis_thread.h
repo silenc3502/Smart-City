e42e292798b94b56a7156f22d2ed7640aba46920
silenc3502 <gcccompil3r@gmail.com>
2022-04-30 02:10:28 +0900

    feat: 프로토콜 분석기 누락분 추가 [TDS-1]

--- smart_city_socket_server/thread/prot_analysis_thread.h
+++ smart_city_socket_server/thread/prot_analysis_thread.h
@@ -1,6 +1,8 @@
 #ifndef SMART_CITY_SOCKET_SERVER_PROT_ANALYSIS_THREAD_H
 #define SMART_CITY_SOCKET_SERVER_PROT_ANALYSIS_THREAD_H
 
+#include "common.h"
+
 void *protocol_analyzer (void *fd);
 
 typedef struct _prot_analysis_metadata prot_analysis_metadata;
@@ -9,8 +11,10 @@ struct _prot_analysis_metadata
 {
     int length;
     int target;
+    int session_id;
     int sub_command;
     int *data;
+    si socket_addr;
 };
 
 #endif //SMART_CITY_SOCKET_SERVER_PROT_ANALYSIS_THREAD_H
