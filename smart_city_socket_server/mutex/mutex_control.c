207e313311e268c6a8c35c7dec29ec62f3295641
silenc3502 <gcccompil3r@gmail.com>
2022-04-30 02:09:26 +0900

    feat: Mutex 초기화 작업 누락분 추가 [EDGSER-1]

--- smart_city_socket_server/mutex/mutex_control.c
+++ smart_city_socket_server/mutex/mutex_control.c
@@ -1,8 +1,10 @@
 #include "mutex_control.h"
 #include "common.h"
 
-extern pthread_mutex_t mtx;
-extern pthread_cond_t edge_recv_cond_mtx;
+#include <pthread.h>
+
+//extern pthread_mutex_t mtx;
+//extern pthread_cond_t edge_recv_cond_mtx;
 
 void init_mutex (void)
 {
