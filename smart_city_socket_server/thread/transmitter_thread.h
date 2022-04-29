1ff1574fb95d7320a1ece11c6ad9f7623137c348
silenc3502 <gcccompil3r@gmail.com>
2022-04-30 02:14:57 +0900

    feat: 송신기 완료 [TDS-1]

---
+++ smart_city_socket_server/thread/transmitter_thread.h
@@ -0,0 +1,16 @@
+#ifndef SMART_CITY_SOCKET_SERVER_TRANSMITTER_THREAD_H
+#define SMART_CITY_SOCKET_SERVER_TRANSMITTER_THREAD_H
+
+#include "common.h"
+
+typedef struct _transmit_data transmit_data;
+
+struct _transmit_data
+{
+    int session_id;
+    si socket_addr;
+};
+
+void *transmitter (void *);
+
+#endif //SMART_CITY_SOCKET_SERVER_TRANSMITTER_THREAD_H
