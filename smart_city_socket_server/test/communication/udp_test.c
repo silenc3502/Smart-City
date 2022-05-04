15105a2a40db52940161430ef5c1fce60a431b17
silenc3502 <gcccompil3r@gmail.com>
2022-05-05 03:01:32 +0900

    feat: UDP 프로토콜 변경 사항 적용 테스트 [GTEST-2]

--- smart_city_socket_server/test/communication/udp_test.c
+++ smart_city_socket_server/test/communication/udp_test.c
@@ -4,6 +4,8 @@
 #include "session_manage.h"
 #include "thread_work_queue.h"
 
+#include "protocol_request_packt.h"
+
 #include <stdbool.h>
 
 void print_receive_data (int *arr)
@@ -21,13 +23,16 @@ void print_receive_data (int *arr)
 bool udp_send_test (void)
 {
     int sock;
+    int session_id;
+
+    char received_ip[16] = { 0 };
     struct sockaddr_in target_addr, from_addr;
     char recv_buffer[1024];
     int recv_len;
     unsigned int address_len;
     socklen_t addr_sz;
     //int data[6] = { 20, TRAFFIC_CONTROL, NO_SESSION, TC_ID_ISSUANCE, 1 };
-    int data[6] = { 20, TRAFFIC_CONTROL, NO_SESSION, TC_ID_ISSUANCE, 0 };
+    struct _protocol_request_packt prp = { 36, TRAFFIC_CONTROL, "192.168.3.6", NO_SESSION, TC_ID_ISSUANCE };
 
     if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
@@ -40,7 +45,7 @@ bool udp_send_test (void)
     target_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     target_addr.sin_port = htons(EDDI_SERVER_PORT);
     address_len = sizeof(target_addr);
-    sendto(sock, &data, 20, 0, (struct sockaddr*)&target_addr, address_len);
+    sendto(sock, &prp, 36, 0, (struct sockaddr*)&target_addr, address_len);
 
     if((recv_len = recvfrom(sock, recv_buffer, 1024, 0, (struct sockaddr *)&target_addr, &address_len)) < 0)
     //if((recv_len = recvfrom(sock, recv_buffer, 1024, 0, (struct sockaddr *)&from_addr, &addr_sz)) < 0)
@@ -54,6 +59,12 @@ bool udp_send_test (void)
     printf("received data : %s\n", recv_buffer);
     print_receive_data((int *)recv_buffer);
     close(sock);
+
+    memcpy(received_ip, &recv_buffer[FOUR_BYTE], IP_ADDR_SIZE);
+    memcpy(&session_id, &recv_buffer[FOUR_BYTE + IP_ADDR_SIZE], FOUR_BYTE);
+
+    printf("received ip: %s\n", received_ip);
+    printf("session id: %d\n", session_id);
 
     return true;
 }
