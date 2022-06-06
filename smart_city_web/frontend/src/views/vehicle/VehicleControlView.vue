a6aee9872da5990353017553b187d240d9d5c40d
silenc3502 <gcccompil3r@gmail.com>
2022-06-07 01:19:40 +0900

    init: 차량 제어 초기화 [VEHICLE-UI]

---
+++ smart_city_web/frontend/src/views/vehicle/VehicleControlView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="header">
+    <h1>Vehicle Control</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VehicleControlView"
+}
+</script>
+
+<style scoped>
+
+</style>
