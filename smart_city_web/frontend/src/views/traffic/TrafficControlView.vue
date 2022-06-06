ad483a9c07fd6685b37e081d37fc9b5b8ad9485c
silenc3502 <gcccompil3r@gmail.com>
2022-06-07 01:22:40 +0900

    init: 교통 제어 초기화 [TRAFFIC-UI]

---
+++ smart_city_web/frontend/src/views/traffic/TrafficControlView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="header">
+    <h1>Traffic Control</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrafficControlView"
+}
+</script>
+
+<style scoped>
+
+</style>
