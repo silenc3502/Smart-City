2d5ebf6833c60b8d7e29cf64d7200269f9bfc281
silenc3502 <gcccompil3r@gmail.com>
2022-06-15 21:43:47 +0900

    feat: 차량 예약 UI 작업 [CARBOOK-UI]

---
+++ smart_city_web/frontend/src/views/board/vehicle/CarBookBoardRegisterView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div><car-book-board-register @submit="onSubmit" /></div>
+</template>
+
+<script>
+import CarBookBoardRegister from "@/components/board/vehicle/CarBookBoardRegister.vue";
+import axios from "axios";
+export default {
+  name: "CarBookBoardRegisterView",
+  components: {
+    CarBookBoardRegister,
+  },
+  methods: {
+    onSubmit(payload) {
+      const { title, content, writer } = payload;
+      axios
+        .post("http://localhost:7777/car-book-list", {
+          title,
+          content,
+          writer,
+        })
+        .then(() => {
+          alert("게시물 등록 성공");
+          this.$router.push({
+            name: "CarBookBoardListView",
+          });
+        })
+        .catch(() => {
+          alert("오류 발생");
+        });
+    },
+  },
+};
+</script>
