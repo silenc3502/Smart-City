13d4f8dd97f8e171c29baf9af38b09506e4e55c9
silenc3502 <gcccompil3r@gmail.com>
2022-06-16 21:43:34 +0900

    feat: 차량 예약 시스템 읽기 UI [CARBOOK-UI]

---
+++ smart_city_web/frontend/src/components/board/vehicle/CarBookBoardRead.vue
@@ -0,0 +1,232 @@
+<template>
+  <div align="center" style="font-family: 'Noto Sans KR', sans-serif">
+    <v-container class="white" style="width: 1200px">
+      <v-row justify="center">
+        <v-col>
+          <v-card>
+            <div class="wrap">
+              <div style="display: flex">
+                <v-text-field
+                  flat
+                  height="100px"
+                  solo
+                  readonly
+                  :value="carBookBoard.title"
+                  single-line
+                  class="title_box"
+                  style="height: 100px"
+                />
+              </div>
+              <v-divider></v-divider>
+              <div style="display: flex; height: 50px" class="mt-3 mb-3">
+                <div>
+                  <v-text-field
+                    flat
+                    solo
+                    height="50px"
+                    readonly
+                    :value="carBookBoard.writer"
+                    class="writer_box"
+                  />
+                </div>
+
+                <div>
+                  <v-text-field
+                    flat
+                    solo
+                    readonly
+                    :value="carBookBoard.regDate"
+                    class="date_box"
+                  />
+                </div>
+                <div class="mt-3">조회 수</div>
+                <div>
+                  <v-text-field
+                    flat
+                    solo
+                    readonly
+                    :value="carBookBoard.viewCount"
+                  />
+                </div>
+                <div
+                  class="mt-2"
+                  v-if="
+                    (carBookBoard.writer == this.nickName &&
+                      this.$store.state.userInfo.auth == '관리자') ||
+                    this.$store.state.userInfo.auth == '매니저'
+                  "
+                >
+                  <v-btn
+                    text
+                    :to="{
+                      name: 'NoticeModifyPage',
+                      params: { boardNo: carBookBoard.id.toString() },
+                    }"
+                    >수정</v-btn
+                  >
+                  <v-dialog v-model="dialogDelete" width="460">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        text
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="restCheckbox"
+                        color="red"
+                      >
+                        삭제
+                      </v-btn>
+                    </template>
+
+                    <v-card>
+                      <v-card-title class="text-h5 orange lighten-3">
+                        게시물 삭제
+                      </v-card-title>
+                      <v-card-text class="mt-5 pb-0">
+                        게시물 삭제를 원하시면 <br />
+                        버튼을 체크하시고 <br />
+                        삭제 버튼을 클릭해주세요.
+                      </v-card-text>
+                      <v-container class="pt-0 pb-0" fluid>
+                        <v-checkbox
+                          color="orange"
+                          v-model="checkbox"
+                          label="삭제 동의 버튼."
+                        ></v-checkbox>
+                      </v-container>
+                      <v-divider></v-divider>
+
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          color="orange lighten-3"
+                          text
+                          :disabled="!checkbox"
+                          @click="onDelete"
+                        >
+                          삭제
+                        </v-btn>
+                        <v-btn
+                          color="orange lighten-3"
+                          text
+                          @click="dialogDelete = false"
+                        >
+                          취소
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </div>
+              </div>
+              <v-divider></v-divider>
+              <div style="display: flex">
+                <v-textarea
+                  flat
+                  solo
+                  readonly
+                  auto-grow
+                  rows="10"
+                  :value="carBookBoard.content"
+                  class="mt-10"
+                  style="font-size: 20px"
+                />
+              </div>
+              <v-btn
+                class="mt-3 mb-3 orange lighten-1"
+                style="float: right"
+                @click="goList"
+                >목록</v-btn
+              >
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: "CarBookBoardRead",
+  props: {
+    carBookBoard: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      userInfo: "",
+      nickName: "",
+      dialogDelete: false,
+      checkbox: false,
+    };
+  },
+  created() {
+    if (this.$store.state.userInfo != null) {
+      this.userInfo = this.$store.state.userInfo;
+      this.nickName = this.userInfo.nickName;
+    } else {
+      alert("로그인 후 이용해주세요.");
+      this.$router.push("/login");
+    }
+  },
+  methods: {
+    goList() {
+      this.$router.push("/noticeList");
+    },
+    onDelete() {
+      const { id } = this.carBookBoard;
+      axios
+        .delete(`http://localhost:7777/noticeBoard/${id}`)
+        .then(() => {
+          alert("삭제 성공!");
+          this.$router.push({ name: "CarBookBoardListView" });
+        })
+        .catch(() => {
+          alert("삭제 실패");
+        });
+    },
+    restCheckbox() {
+      this.checkbox = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap");
+
+.table {
+  border: 1px solid black;
+}
+.title_box {
+  font-size: 32px;
+  zoom: 1.1;
+}
+.writer_box {
+  font-size: 21px;
+  zoom: 0.9;
+}
+.date_box {
+  font-size: 18px;
+  color: var(--skinText979797);
+  zoom: 0.9;
+  margin-top: 2px;
+}
+.wrap {
+  margin: 30px;
+}
+.main_tit_box {
+  text-align: center;
+  font-size: 35px;
+  font-weight: bold;
+  color: #333;
+  padding: 8px 0 8px 0;
+  position: relative;
+}
+
+</style>
---
+++ smart_city_web/frontend/src/views/board/vehicle/CarBookBoardReadView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <car-book-board-read v-if="carBookBoard" :carBookBoard="carBookBoard" />
+  </div>
+</template>
+
+<script>
+import CarBookBoardRead from "@/components/board/vehicle/CarBookBoardRead.vue";
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "CarBookBoardReadView",
+  components: {
+    CarBookBoardRead,
+  },
+  props: {
+    boardNo: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapState(["carBookBoard"]),
+  },
+  created() {
+    this.fetchCarBookBoard(this.boardNo).catch(() => {
+      alert("게시물 요청 실패");
+      this.$router.push();
+    });
+  },
+  methods: {
+    ...mapActions(["fetchCarBookBoard"]),
+  },
+};
+</script>
