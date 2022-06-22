af5a1051c1125b3b9984f961903b6d1bc0a85d73
silenc3502 <gcccompil3r@gmail.com>
2022-06-22 21:26:08 +0900

    feat: 레디스 설정만 살림 [REDIS-INIT]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/member/MemberController.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/member/MemberController.java
@@ -4,26 +4,16 @@ import kr.eddi.smartcity.controller.member.form.EmailMatchPhoneForm;
 import kr.eddi.smartcity.controller.member.form.EmailPasswordForm;
 import kr.eddi.smartcity.controller.member.form.MemberLoginForm;
 import kr.eddi.smartcity.controller.member.form.MemberRegisterForm;
-import kr.eddi.smartcity.security.jwt.dto.JwtDto;
-import kr.eddi.smartcity.security.jwt.service.JwtAuthServiceImpl;
+
 import kr.eddi.smartcity.service.account.SignInService;
 import kr.eddi.smartcity.service.account.SignUpService;
-import kr.eddi.smartcity.service.account.dto.MemberLoginRequest;
-import kr.eddi.smartcity.service.session.SessionService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -34,10 +24,6 @@ public class MemberController {
 
     private final SignUpService signUpService;
     private final SignInService signInService;
-    private final JwtService jwtService;
-    private final RedisServiceImpl redisService;
-    private final AuthenticationManager authenticationManager;
-    private final JwtAuthServiceImpl jwtAuthService;
 
     @PostMapping("/sign-up")
     public Boolean signUp(@Validated @RequestBody MemberRegisterForm form, BindingResult bindingResult) {
@@ -66,25 +52,12 @@ public class MemberController {
     }
 
     @PostMapping("/sign-in")
-    public JwtDto signIn(@Validated @RequestBody MemberLoginForm form, BindingResult bindingResult) {
+    public Boolean signIn(@Validated @RequestBody MemberLoginForm form, BindingResult bindingResult) {
         log.info("MainFormController#signIn: {}", form);
-        /*
-        if (bindingResult.hasFieldErrors()) {
-            return false;
-        }
+
         signInService.signIn(form.toLoginRequest());
-        return true; */
-        String email = form.getEmail();
-        String password = form.getPassword();
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        JwtDto jwtDto = jwtAuthService.createToken(email);
-
-        return jwtDto;
+        return true;
     }
 
     @GetMapping("/find-email/{email}")
