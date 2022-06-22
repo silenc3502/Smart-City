d2460aaa028098e894694b17dd9f878faa6f85ac
silenc3502 <gcccompil3r@gmail.com>
2022-06-22 21:26:08 +0900

    feat: 레디스 설정만 살림 [REDIS-INIT]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/member/MainPageController.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/member/MainPageController.java
@@ -1,13 +1,8 @@
 package kr.eddi.smartcity.controller.member;
 
-import kr.eddi.smartcity.service.account.ResignService;
-import kr.eddi.smartcity.service.session.SessionKey;
 import kr.eddi.smartcity.service.session.SessionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
@@ -15,11 +10,9 @@ import javax.servlet.http.HttpSession;
 @CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
 public class MainPageController {
     private final SessionService sessionService;
-    private final ResignService resignService;
 
-    public MainPageController(SessionService sessionService, ResignService resignService) {
+    public MainPageController(SessionService sessionService) {
         this.sessionService = sessionService;
-        this.resignService = resignService;
     }
 
     @GetMapping("/logout")
@@ -32,27 +25,4 @@ public class MainPageController {
     public void checkLogin(@RequestParam("email") String email) {
         log.info("session find(): " + sessionService.find());
     }
-
-    /*
-    @GetMapping("/resign")
-    public void resignMember(HttpServletRequest request) {
-
-        log.info("session find: " + sessionService.get());
-
-        HttpSession session = request.getSession();
-        String email;
-
-        try{
-            email = (String) session.getAttribute((SessionKey.SESSION_USER).name());
-
-            log.info("email: " + email);
-
-            resignService.resign(email);
-        } catch(Exception ex) {
-            email = null;
-        }
-
-        //String email = sessionService.checkResignMember();
-    }
-     */
 }
--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/security/jwt/service/RedisService.java
+++
@@ -1,8 +0,0 @@
-package kr.eddi.smartcity.security.jwt.service;
-
-public interface RedisService {
-
-    public void setKeyAndValue(String token, Long memNo);
-    public Long getValueByKey(String email);
-    public void deleteByKey(String email);
-}
