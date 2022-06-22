af39e07731c66ab7a562d8bea379dc4910a23b70
silenc3502 <gcccompil3r@gmail.com>
2022-06-22 21:26:08 +0900

    feat: 레디스 설정만 살림 [REDIS-INIT]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/SmartcityBackendApplication.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/SmartcityBackendApplication.java
@@ -1,14 +1,17 @@
 package kr.eddi.smartcity;
 
-import kr.eddi.smartcity.utility.network.MainServerSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.IOException;
 
 @EnableJpaAuditing
 @SpringBootApplication
+//@EnableConfigurationProperties(AppProperties.class)
+//@EnableCaching
 public class SmartcityBackendApplication {
 
     public static void main(String[] args) throws IOException {
