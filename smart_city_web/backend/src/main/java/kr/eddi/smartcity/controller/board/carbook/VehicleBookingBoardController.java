62a269ffa4b03870c720f529b2bc142759576414
silenc3502 <gcccompil3r@gmail.com>
2022-06-23 21:35:05 +0900

    feat: 차량 예약 시스템 읽기 완료, 수정 준비중 [SYS-CARBOOK]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/board/carbook/VehicleBookingBoardController.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/controller/board/carbook/VehicleBookingBoardController.java
@@ -3,6 +3,7 @@ package kr.eddi.smartcity.controller.board.carbook;
 import kr.eddi.smartcity.controller.board.carbook.form.VehicleBookingBoardForm;
 import kr.eddi.smartcity.entity.board.carbook.VehicleBookingBoard;
 import kr.eddi.smartcity.service.board.vehicle.VehicleBookingBoardService;
+import kr.eddi.smartcity.service.security.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,32 +17,63 @@ import java.util.List;
 public class VehicleBookingBoardController {
 
     private final VehicleBookingBoardService service;
+    private final RedisService redisService;
 
-    public VehicleBookingBoardController(VehicleBookingBoardService service) {
+    public VehicleBookingBoardController(VehicleBookingBoardService service, RedisService redisService) {
         this.service = service;
+        this.redisService = redisService;
     }
 
     @PostMapping("/register")
     public void vehicleBookingBoardRegister(@Validated @RequestBody VehicleBookingBoardForm form){
-        log.info("vehicleBookingBoardRegister(): " + form.getEmail());
+        log.info("vehicleBookingBoardRegister(): " + form.getToken());
 
-        service.register(form.toVehicleBookingBoardRequest());
+        String token = form.getToken();
+        Long memberId = redisService.getValueByKey(token);
+
+        log.info("memberId: " + memberId);
+
+        service.register(form.toVehicleBookingBoardRequest(memberId));
     }
 
-    @GetMapping("/list")
-    public List<VehicleBookingBoard> vehicleBookingBoardList(){
+    @PostMapping("/list")
+    public List<VehicleBookingBoard> vehicleBookingBoardList(@RequestBody String token){
         log.info("vehicleBookingBoardList()");
 
-        log.info("list(): " + service.list().get(0).toString());
+        //log.info("list(): " + service.list().get(0).toString());
 
-        return service.list();
+        token = token.substring(0, token.length() - 1);
+        Long memberId = redisService.getValueByKey(token);
+
+        return service.list(memberId);
     }
 
     @GetMapping("/{boardNo}")
     public VehicleBookingBoard vehicleBookingBoardRead (
             @PathVariable("boardNo") Integer boardNo) {
+
         log.info("vehicleBookingBoardRead()");
 
         return service.read(boardNo);
+    }
+
+    @PutMapping("/{boardNo}")
+    public VehicleBookingBoard noticeBoardModify(
+            @PathVariable("boardNo")Long boardNo,
+            @RequestBody VehicleBookingBoard vehicleBookingBoard) {
+
+        log.info("noticeBoardModify(): " + vehicleBookingBoard.getId());
+
+        //vehicleBookingBoard.setBoardNo(boardNo);
+        service.modify(vehicleBookingBoard);
+
+        return vehicleBookingBoard;
+    }
+
+    @DeleteMapping("/{boardNo}")
+    public void noticeBoardRemove(@PathVariable("boardNo") Long boardNo){
+        log.info("noticeBoardRemove()");
+
+        service.remove(boardNo);
     }
 }
