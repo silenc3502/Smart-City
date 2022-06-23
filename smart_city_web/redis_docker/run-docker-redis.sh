8ff61c5ba2d9668213fa1a3a624ba893b187a09e
silenc3502 <gcccompil3r@gmail.com>
2022-06-24 01:52:13 +0900

    feat: 도커상에서 레디스 재구동시 발생하는 문제 해결 [DOCKER-REDIS]

--- smart_city_web/redis_docker/run-docker-redis.sh
+++ smart_city_web/redis_docker/run-docker-redis.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
+docker_check=$(docker ps -a | grep "redis" | awk '{ print $1 }')
+if [ -n "$docker_check" ];
+then
+	echo "redis 컨테이너를 내립니다."
+	docker rm $docker_check
+fi
+
+echo "redis 컨테이너를 구동합니다."
 docker-compose -f ./docker-redis-server.yml up -d
