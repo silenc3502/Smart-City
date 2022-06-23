c6e3cd0e39964747668d2ef58a5cd75120ce58bc
silenc3502 <gcccompil3r@gmail.com>
2022-06-24 01:53:54 +0900

    feat: 차량 예약 시스템 작업 완료 [SYS-CARBOOK]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/entity/common/BaseTimeEntity.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/entity/common/BaseTimeEntity.java
@@ -1,6 +1,7 @@
 package kr.eddi.smartcity.entity.common;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseTimeEntity {
 
+    @Setter
     @CreatedDate
     private LocalDateTime createdDate;
 
