7ae8876dd878afa83fa9369a78682a6afc8ea0c3
CodingBoxer <thsvygns123@gmail.com>
2022-08-05 02:33:05 +0900

    feat : spi_open()시 요청한 SPIREG의 사용 가능 여부 판정[COMMON-INTF6]

--- traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_object/source/spi_obj_manager.c
+++ traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_object/source/spi_obj_manager.c
@@ -37,11 +37,19 @@ struct _spi_dev{
 spi_dev *spi_dev_t[SPI_TOT_NUM];
 spiBASE_t *spi_reg[SPI_TOT_NUM] = {spiREG1, spiREG2, spiREG3, spiREG4, spiREG5};
 
-void spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
+boolean spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
 {
+    if(spi_num > SPI_TOT_NUM)
+        return FALSE;
+
     spi_dev_t[spi_num] = (spi_dev *)malloc(sizeof(spi_dev));
+
+    if(spi_dev_t[spi_num] == NULL)
+        return FALSE;
+
     spi_dev_t[spi_num]->register_t.base = spi_reg[spi_num];
     //센서 종류에 해당하는 read, write, io_ctl 함수 등록
+    return TRUE;
 }
 
 void spi_close(const spi_dev_num spi_num)
