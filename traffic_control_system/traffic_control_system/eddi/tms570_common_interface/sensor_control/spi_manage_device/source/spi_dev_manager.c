085458b2d06e152d59ca4c2a7f91ef553f375341
CodingBoxer <thsvygns123@gmail.com>
2022-08-05 22:31:44 +0900

    issue : SPI 레지스터 구조체 분리[COMMON-INTF6]

--- traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
+++ traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
@@ -8,16 +8,11 @@
 #include "spi_operation_manager.h"
 #include "stdlib.h"
 
-typedef struct _spi_register spi_register;
-struct _spi_register{
-    spiBASE_t *base;
-    spiDAT1_t data_config;
-};
-
 typedef struct _spi_dev spi_dev;
 struct _spi_dev{
+    spiBASE_t *spi_reg;
+
     spi_operations *operations;
-    spi_register register_t;
 };
 spi_dev *spi_dev_t[SPI_TOT_NUM];
 spiBASE_t *spi_reg[SPI_TOT_NUM] = {spiREG1, spiREG2, spiREG3, spiREG4, spiREG5};
@@ -32,8 +27,8 @@ boolean spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
     if(spi_dev_t[spi_num] == NULL)
         return FALSE;
 
-    spi_dev_t[spi_num]->register_t.base = spi_reg[spi_num];
-    //센서 종류에 해당하는 read, write, io_ctl 함수 등록
+    spi_dev_t[spi_num]->spi_reg = spi_reg[spi_num];
+
     return TRUE;
 }
 
