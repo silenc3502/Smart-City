122ed0d9c2de8f45bbf7e8255beb44aa01a76ec7
CodingBoxer <thsvygns123@gmail.com>
2022-08-06 11:42:03 +0900

    issue : SPI 장치 식별[COMMON-INTF6]

--- traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
+++ traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
@@ -17,19 +17,13 @@ struct _spi_dev{
 spi_dev *spi_dev_t[SPI_TOT_NUM];
 spiBASE_t *spi_reg[SPI_TOT_NUM] = {spiREG1, spiREG2, spiREG3, spiREG4, spiREG5};
 
-boolean spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
+spiBASE_t *spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
 {
-    if(spi_num > SPI_TOT_NUM)
-        return FALSE;
-
     spi_dev_t[spi_num] = (spi_dev *)malloc(sizeof(spi_dev));
-
-    if(spi_dev_t[spi_num] == NULL)
-        return FALSE;
 
     spi_dev_t[spi_num]->spi_reg = spi_reg[spi_num];
 
-    return TRUE;
+    return spi_dev_t[spi_num]->spi_reg;
 }
 
 void spi_close(const spi_dev_num spi_num)
