c4e7f32dfe762a9e8d27a5c5155fd06742ca3dfb
CodingBoxer <thsvygns123@gmail.com>
2022-08-27 16:39:28 +0900

    issue : 고정된 개수의 SPI 사용에 따른 장치 관리객체 동적할당 구조 변경[COMMON-INTF6]

--- traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
+++ traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
@@ -6,22 +6,26 @@
  */
 #include "spi_dev_manager.h"
 #include "spi_operation_manager.h"
+#include "spi_status_manager.h"
 #include "stdlib.h"
 
 typedef struct _spi_dev spi_dev;
 struct _spi_dev{
+    sensor_dev_name sensor_type;
     spiBASE_t *spi_reg;
 
+    spi_status *status;
     spi_operations *operations;
 };
-spi_dev *spi_dev_t[SPI_TOT_NUM];
+spi_dev spi_dev_t[SPI_TOT_NUM];
 spiBASE_t *spi_reg[SPI_TOT_NUM] = {spiREG1, spiREG2, spiREG3, spiREG4, spiREG5};
 
 spiBASE_t *spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
 {
-    spi_dev_t[spi_num] = (spi_dev *)malloc(sizeof(spi_dev));
-
     spi_dev_t[spi_num]->spi_reg = spi_reg[spi_num];
+    spi_dev_t[spi_num]->sensor_type = dev_name;
+
+    //operations와 status의 정보를 가지고 온다
 
     return spi_dev_t[spi_num]->spi_reg;
 }
