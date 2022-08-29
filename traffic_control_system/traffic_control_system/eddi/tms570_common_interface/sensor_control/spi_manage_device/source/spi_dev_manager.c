887297a512552b1937841c592c8ac8d639d7349d
CodingBoxer <thsvygns123@gmail.com>
2022-08-29 21:44:52 +0900

    issue : SPI 레지스터 사용가능 여부 판정[COMMON-INTF6]

--- traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
+++ traffic_control_system/traffic_control_system/eddi/tms570_common_interface/sensor_control/spi_manage_device/source/spi_dev_manager.c
@@ -9,19 +9,23 @@
 #include "spi_status_manager.h"
 #include "stdlib.h"
 
+#define REG_ALLOC_ERR -1
+
 typedef struct _spi_dev spi_dev;
 struct _spi_dev{
     sensor_dev_name sensor_type;
     spiBASE_t *spi_reg;
-
     spi_status *status;
     spi_operations *operations;
 };
 spi_dev spi_dev_t[SPI_TOT_NUM];
 spiBASE_t *spi_reg[SPI_TOT_NUM] = {spiREG1, spiREG2, spiREG3, spiREG4, spiREG5};
 
-uint32_t spi_open(spi_dev_num spi_num, const sensor_dev_name dev_name)
+int32_t spi_open(const spi_dev_num spi_num, const sensor_dev_name dev_name)
 {
+    if(spi_dev_t[spi_num].spi_reg == spi_reg[spi_num])
+        return REG_ALLOC_ERR;
+
     spi_dev_t[spi_num]->spi_reg = spi_reg[spi_num];
     spi_dev_t[spi_num]->sensor_type = dev_name;
 
