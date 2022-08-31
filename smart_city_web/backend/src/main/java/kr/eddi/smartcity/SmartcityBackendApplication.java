80a6fe9da99a376cd14962ae74110852b96b21dd
silenc3502 <gcccompil3r@gmail.com>
2022-08-31 13:43:31 +0900

    Merge pull request #68 from silenc3502/main
    feat: 회원 관리 시스템 [WEB-MEMBER]

--- smart_city_web/backend/src/main/java/kr/eddi/smartcity/SmartcityBackendApplication.java
+++ smart_city_web/backend/src/main/java/kr/eddi/smartcity/SmartcityBackendApplication.java
@@ -2,6 +2,7 @@ package kr.eddi.smartcity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
