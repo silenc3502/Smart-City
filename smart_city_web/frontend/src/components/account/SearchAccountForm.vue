b038f30d25ca7b60fdebf5b2a9cec8af2a824087
silenc3502 <gcccompil3r@gmail.com>
2022-08-31 13:20:58 +0900

    feat: 사용자 계정 정보 확인 [MEMBER-UI]

--- smart_city_web/frontend/src/components/account/SearchAccountForm.vue
+++ smart_city_web/frontend/src/components/account/SearchAccountForm.vue
@@ -5,44 +5,39 @@
         <v-col cols="auto" style="padding-bottom: 90px">
           <router-link to="/">
             <v-img
-                :src="require('@/assets/img/logo.png')"
+                :src="require('@/assets/logo.png')"
                 width="120"
                 class="mx-auto mb-6"
             ></v-img>
           </router-link>
           <v-card width="460">
             <v-card-text class="text-center px-12 py-16">
-              <validation-observer ref="observer" v-slot="{ invalid }">
-                <v-form @submit.prevent="onSubmit">
-                  <div class="text-h4 font-weight-black mb-10">ID 찾기</div>
 
-                  <validation-provider
-                      v-slot="{ errors }"
-                      name="이메일"
-                      :rules="{ email: true, required: true }"
-                  >
-                    <v-text-field
-                        v-model="email"
-                        label="이메일"
-                        clearable
-                        prepend-icon="mdi-email"
-                        :error-messages="errors"
-                        color="orange"
-                    />
-                  </validation-provider>
-                  <v-btn
+              <v-form @submit.prevent="onSubmit">
+                <div class="text-h4 font-weight-black mb-10">ID 찾기</div>
+
+                <div class="d-flex">
+                  <v-text-field
+                      v-model="email"
+                      label="이메일"
+                      :rules="email_rule"
+                      clearable
+                      prepend-icon="mdi-email"
+                      color="orange"
+                  />
+                </div>
+
+                <v-btn
                       block
                       x-large
                       rounded
                       color="orange lighten-1"
                       class="mt-6"
-                      @click="findUserId"
-                      :disabled="invalid"
-                  >
-                    찾기</v-btn
-                  >
-                </v-form>
-              </validation-observer>
+                      @click="findEmailAccount"
+                      :disabled="false"
+                  >찾기</v-btn>
+              </v-form>
+
             </v-card-text>
           </v-card>
         </v-col>
@@ -60,20 +55,27 @@ export default {
   data() {
     return {
       email: "",
+      email_rule:[
+        v => !!v || '이메일을 입력해주세요.',
+        v => {
+          const replaceV = v.replace(/(\s*)/g, '')
+          const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+          return pattern.test(replaceV) || '이메일 형식으로 입력해주세요'
+        }
+      ],
     };
   },
   methods: {
-    findUserId() {
+    findEmailAccount() {
       const { email } = this;
-      axios
-          .post("http://localhost:7777/member/findUserId", { email })
+      axios.get(`http://localhost:7777/member/find-email/${email}`)
           .then((res) => {
-            this.userId = res.data;
-            if (this.userId != "") {
-              alert(`아이디는 ${this.userId} 입니다.`);
-              this.$router.push("/login");
+            this.temp = res.data;
+            if (res.data) {
+              alert("사용 가능한 이메일 입니다.");
+              this.$router.push("/sign-in");
             } else {
-              alert("찾으시는 아이디가 없습니다.");
+              alert("가입 이력이 없습니다.");
             }
           });
     },
