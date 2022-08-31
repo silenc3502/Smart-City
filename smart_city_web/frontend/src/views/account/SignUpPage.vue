bde4ead3466fd6bb1f646031a0d7be0753c0dad7
silenc3502 <gcccompil3r@gmail.com>
2022-08-31 13:15:32 +0900

    feat: 회원 가입 UI [MEMBER-UI]

---
+++ smart_city_web/frontend/src/views/account/SignUpPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <sign-up-form @submit="onSubmit"></sign-up-form>
+  </div>
+</template>
+
+<script>
+
+import SignUpForm from "@/components/account/SignUpForm.vue";
+import axios from "axios";
+
+export default {
+  name: "SignUpPage",
+  components: {
+    SignUpForm,
+  },
+  methods: {
+    onSubmit(payload) {
+      const { id, pw, name, nickName, email, auth } = payload;
+      axios
+          .post("http://localhost:7777/member/sign-up", {
+            id, pw, name, nickName, email, auth,
+          })
+          .then((res) => {
+            alert("회원가입 완료" + res);
+            this.$router.push("/login");
+          })
+          .catch((res) => {
+            alert(res.response.data.message);
+          });
+    },
+  },
+};
+
+</script>
+
+<style scoped>
+
+</style>
