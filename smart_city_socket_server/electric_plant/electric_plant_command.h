944202cb9f97da1209502b738b9d03b67b4a4148
silenc3502 <gcccompil3r@gmail.com>
2022-09-20 14:40:32 +0900

    feat: 전력 그리드 서브 프로토콜 작성 [SOCKET-SERVER20]

--- smart_city_socket_server/electric_plant/electric_plant_command.h
+++ smart_city_socket_server/electric_plant/electric_plant_command.h
@@ -2,12 +2,14 @@
 #define SMART_CITY_SOCKET_SERVER_ELECTRIC_PLANT_COMMAND_H
 
 enum eddi_smartcity_electric_plant_protocol {
-    EP_ID_ISSUANCE = 1,
-    EP_END
+    ELECTRIC_PLANT_ID_ISSUANCE = 1,
+    ELECTRIC_PLANT_POWER_CONTROL = 2,
+    ELECTRIC_PLANT_BATTERY_STATUS = 3,
+    ELECTRIC_PLANT_END
 };
 
-#define EP_CALL_BUFFER_COUNT          (EP_END)
-#define EP_CALL_BUFFER                ((EP_END) - (1))
+#define EP_CALL_BUFFER_COUNT          (ELECTRIC_PLANT_END)
+#define EP_CALL_BUFFER                ((ELECTRIC_PLANT_END) - (1))
 
 typedef void (* electric_plant_table_ptr_t) (void *);
 
