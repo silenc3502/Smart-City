1a962b4e0bacfb7173db7512f7ff286863bda7a2
silenc3502 <gcccompil3r@gmail.com>
2022-09-24 11:02:36 +0900

    feat: 도로 통제 시스템 아이디 발급 메커니즘 완료 [SOCKET-SERVER21]

--- smart_city_socket_server/traffic_control/tc_command_handler.c
+++ smart_city_socket_server/traffic_control/tc_command_handler.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
+
+#include <arpa/inet.h>
 
 #include "protocol_request_packt.h"
 #include "tc_command_handler.h"
@@ -14,6 +17,9 @@
 
 #include "thread_work_queue.h"
 
+#define SESSION_IDX         0
+#define SESSION_SIZE        4
+
 void tc_dummy (void *pkt)
 {
     printf("미구현 스펙입니다!\n");
@@ -24,35 +30,27 @@ void tc_id_issuance (void *pkt)
 {
     int *command_data = ((prot_analysis_metadata *)pkt)->data;
     int target_command = ((prot_analysis_metadata *)pkt)->target;
-    int session_id = ((prot_analysis_metadata *)pkt)->session_id;
+    int session_id;
 
     si socket_addr = ((prot_analysis_metadata *)pkt)->socket_addr;
     int alloc_session;
 
     transmit_data *data;
 
-    printf("아이디(세션) 발급 커맨드\n");
+    memmove(&session_id, &command_data[SESSION_IDX], SESSION_SIZE);
+    printf("현재 세션 아이디: %d\n", session_id);
 
-    if (command_data[0])
-    {
-        printf("아이디(세션) 수동 발급\n");
-    }
-    else
-    {
-        printf("아이디(세션) 자동 발급\n");
-        // TODO: socket_manage_map 검색 및 발급
-        if (session_id == NO_SESSION)
-        {
-            alloc_session = request_session_id(target_command, session_id);
-        }
+    // TODO: socket_manage_map 검색 및 발급
+    alloc_session = request_session_id(target_command, session_id);
+    printf("아이디(세션) 자동 발급: %d\n", alloc_session);
 
-        data = (transmit_data *)malloc(sizeof(transmit_data));
-        data->session_id = alloc_session;
-        data->socket_addr = socket_addr;
-        memcpy(data->dest_ip_addr, ((prot_analysis_metadata *)pkt)->ip_addr, IP_ADDR_SIZE);
+    data = (transmit_data *)malloc(sizeof(transmit_data));
+    data->session_id = alloc_session;
+    data->socket_addr = socket_addr;
 
-        enqueue_node_data(&transmit_queue, data);
-    }
+    inet_ntop(AF_INET, &(socket_addr.sin_addr), data->dest_ip_addr, INET_ADDRSTRLEN);
+
+    enqueue_node_data(&transmit_queue, data);
 }
 
 void tc_barricade_handler (void *pkt)
