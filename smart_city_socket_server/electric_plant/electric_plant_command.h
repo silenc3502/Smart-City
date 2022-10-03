a273bab8b1857cfc71ce877727dccd9bfe47e217
silenc3502 <gcccompil3r@gmail.com>
2022-10-04 00:56:59 +0900

    feat: 전력 그리드 시스템의 아이디 발급 처리 [SOCKET_SERVER25]

--- smart_city_socket_server/electric_plant/electric_plant_command.h
+++ smart_city_socket_server/electric_plant/electric_plant_command.h
@@ -3,22 +3,16 @@
 
 enum eddi_smartcity_electric_plant_protocol {
     ELECTRIC_PLANT_ID_ISSUANCE = 1,
-    ELECTRIC_PLANT_POWER_CONTROL = 2,
-    ELECTRIC_PLANT_OUTPUT_CURRENT_STATUS = 3,
+    ELECTRIC_PLANT_EDGE_POWER_CONTROL = 2,
+    ELECTRIC_PLANT_EACH_OUTPUT_CURRENT_STATUS = 3,
     ELECTRIC_PLANT_BATTERY_EACH_CELL_VOLTAGE_STATUS = 4,
     ELECTRIC_PLANT_BATTERY_MODULE_TEMPERATURE_STATUS = 5,
     ELECTRIC_PLANT_BATTERY_HEALTH_STATUS = 6,
+    ELECTRIC_PLANT_BATTERY_CHARGE = 7,
     ELECTRIC_PLANT_END
 };
 
 #define EP_CALL_BUFFER_COUNT          (ELECTRIC_PLANT_END)
 #define EP_CALL_BUFFER                ((ELECTRIC_PLANT_END) - (1))
-
-typedef void (* electric_plant_table_ptr_t) (void *);
-
-const electric_plant_table_ptr_t electric_plant_command_table[EP_CALL_BUFFER_COUNT] = {
-        [0 ... EP_CALL_BUFFER] = NULL,
-#include "electric_plant_command_handler.h"
-};
 
 #endif //SMART_CITY_SOCKET_SERVER_ELECTRIC_PLANT_COMMAND_H
--- smart_city_socket_server/electric_plant/electric_plant_command_table.h
+++ smart_city_socket_server/electric_plant/electric_plant_command_table.h
@@ -1,11 +1,13 @@
 #ifndef SMART_CITY_SOCKET_SERVER_ELECTRIC_PLANT_COMMAND_TABLE_H
 #define SMART_CITY_SOCKET_SERVER_ELECTRIC_PLANT_COMMAND_TABLE_H
 
-#define __EP_COMMAND_TABLE(nr, sym) [nr] = sym,
+#include "electric_plant_command.h"
 
-#include "electric_plant_command_handler.h"
+typedef void (* electric_plant_table_ptr_t) (void *);
 
-__EP_COMMAND_TABLE(0, electric_plant_dummy)
-__EP_COMMAND_TABLE(1, electric_plant_id_issuance)
+const electric_plant_table_ptr_t electric_plant_command_table[EP_CALL_BUFFER_COUNT] = {
+        [0 ... EP_CALL_BUFFER] = NULL,
+#include "electric_plant_command_table_map.h"
+};
 
 #endif //SMART_CITY_SOCKET_SERVER_ELECTRIC_PLANT_COMMAND_TABLE_H
