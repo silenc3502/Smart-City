03389a170ab1ec6ee98523cf5f272a3fbcb6e308
silenc3502 <gcccompil3r@gmail.com>
2022-10-06 11:34:32 +0900

    feat: 전력 그리드의 모듈 온도 정보 관리에서 발생한 누락 사항 추가 [SOCKET_SERVER35]

--- smart_city_socket_server/database/redis/redis_command_protocol_handler.c
+++ smart_city_socket_server/database/redis/redis_command_protocol_handler.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 
 #include "redis_command_protocol_handler.h"
+#include "redis_manage.h"
 
 #include "in_memory_record_thread.h"
 
@@ -19,5 +20,17 @@ void redis_command_dummy (void *metadata)
 
 void redis_command_battery_module_temperature (void *metadata)
 {
+    char buf[128] = { 0 };
+    in_memory_db_proc_metadata *in_memory_data = metadata;
     printf("배터리 모듈 온도 데이터 처리\n");
+
+    sprintf(buf, "%f %f", in_memory_data->data[0], in_memory_data->data[1]);
+    printf("buf = %s\n", buf);
+    redis_reply = redisCommand(redis_context, "sadd %s %s", in_memory_data->table_name, buf);
+    printf("RESPONSE: %s\n", redis_reply->str);
+    freeReplyObject(redis_reply);
+
+    redis_reply = redisCommand(redis_context, "SMEMBERS %s", "battery_contact_temperature");
+    printf("RESPONSE: %s\n", redis_reply->str);
+    freeReplyObject(redis_reply);
 }
--- smart_city_socket_server/database/redis/redis_manage.c
+++ smart_city_socket_server/database/redis/redis_manage.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-#include <hiredis/hiredis.h>
 #include "redis_manage.h"
 
 int redis_connect(char *ip, int port)
 {
-    redis_context = redisConnect("127.0.0.1", 6379);
+    redis_context = redisConnect(ip, port);
     if (redis_context->err) {
         printf("error: %s\n", redis_context->errstr);
         return CONNECT_ERROR;
