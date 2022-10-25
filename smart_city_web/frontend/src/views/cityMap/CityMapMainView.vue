471a02ece10de5387544f2a162be7ce8503b60ba
Sanghoon Lee <gcccompil3r@gmail.com>
2022-10-25 21:34:00 +0900

    feat: 도시 지형 Web UI 생성 [CITY-WEB11]

--- smart_city_web/frontend/src/views/cityMap/CityMapMainView.vue
+++ smart_city_web/frontend/src/views/cityMap/CityMapMainView.vue
@@ -10,12 +10,18 @@ export default {
   name: "CityMapMainView.vue",
   data() {
     return {
+      x_axis: null,
+      y_axis: null,
+      z_axis: null,
+      power_grid: null,
+      plane: null,
       cube: null,
       renderer: null,
       scene: null,
       camera: null,
       navbarHeight: null,
-      rendererHeight: null
+      rendererHeight: null,
+      directLight: null,
     }
   },
   methods: {
@@ -39,19 +45,68 @@ export default {
       this.renderer.setSize(window.innerWidth, window.innerHeight)
       document.body.appendChild(this.renderer.domElement)
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1)
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+      const geometry = new THREE.BoxGeometry(3, 3, 3)
+      const material = new THREE.MeshBasicMaterial({ color: 0x00bb00 })
       this.cube = new THREE.Mesh(geometry, material)
+      //this.cube.position.y = 10
+      this.cube.position.z = 10
       this.scene.add(this.cube)
-
-      this.camera.position.z = 5
 
       document.getElementsByClassName('v-main__wrap')[0].childNodes[1].appendChild(this.renderer.domElement)
 
       this.navbarHeight = document.getElementsByClassName('v-main__wrap')[0].childNodes[0].childNodes[0].clientHeight
       this.rendererHeight = this.renderer.domElement.clientHeight - this.navbarHeight
 
+      // TODO: Need to make Component
+      this.renderer.setClearColor(new THREE.Color(0xCC, 0xCC, 0xCC));
       this.renderer.setSize(this.renderer.domElement.clientWidth, this.rendererHeight, true);
+
+      //const planeGeometry = new THREE.PlaneGeometry(window.innerWidth - 1120, 80, 1, 1);
+      const planeGeometry = new THREE.PlaneGeometry(this.renderer.domElement.clientWidth, this.rendererHeight, 1, 1);
+      const planeMaterial = new THREE.MeshBasicMaterial({color: 0xbbbbbb});
+      this.plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      this.plane.receiveShadow = true;
+      this.scene.add(this.plane)
+
+      const power_grid_geometry = new THREE.BoxGeometry(20, 20, 6)
+      const power_grid_material = new THREE.MeshLambertMaterial({ color: 0xFFE0B2 })
+      this.power_grid = new THREE.Mesh(power_grid_geometry, power_grid_material)
+      //this.cube.position.y = 10
+
+      this.power_grid.position.x = 15
+      this.power_grid.position.y = 25
+      this.power_grid.position.z = 20
+      this.scene.add(this.power_grid)
+
+      const x_axis_geometry = new THREE.BoxGeometry(1, 50, 1)
+      const x_axis_material = new THREE.MeshLambertMaterial({ color: 0xFFE0B2 })
+      this.x_axis = new THREE.Mesh(x_axis_geometry, x_axis_material)
+      this.x_axis.rotation.x = Math.PI / 2
+      this.scene.add(this.x_axis)
+
+      const y_axis_geometry = new THREE.BoxGeometry(1, 50, 1)
+      const y_axis_material = new THREE.MeshLambertMaterial({ color: 0xFFE0B2 })
+      this.y_axis = new THREE.Mesh(y_axis_geometry, y_axis_material)
+      this.y_axis.rotation.y = Math.PI / 2
+      this.scene.add(this.y_axis)
+
+      const z_axis_geometry = new THREE.BoxGeometry(1, 50, 1)
+      const z_axis_material = new THREE.MeshLambertMaterial({ color: 0xFFE0B2 })
+      this.z_axis = new THREE.Mesh(z_axis_geometry, z_axis_material)
+      this.z_axis.rotation.z = Math.PI / 2
+      this.scene.add(this.z_axis)
+
+      this.directLight = new THREE.DirectionalLight(0xffffff, 0.5);
+      this.directLight.castShadow = true;
+
+      this.spotLight = new THREE.SpotLight(0xffffff);
+      this.spotLight.position.set(-30, -20, 70);
+      this.spotLight.castShadow = true;
+      this.scene.add(this.spotLight);
+
+      this.camera.position.z = 60
+      this.camera.position.y = 10
+      this.camera.position.x = -20
 
       window.addEventListener( 'resize', this.onWindowResize, false );
     },
